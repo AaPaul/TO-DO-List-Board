5002f88e121fa4ab9103d1c125a136b986c3eff6
AaPaul <aapaulcn@gmail.com>
2022-03-08 15:42:12 -0500

    update

--- todo_proj/settings.py
+++ todo_proj/settings.py
@@ -79,19 +79,19 @@ WSGI_APPLICATION = 'todo_proj.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'TODO_Proj',
-    #     'USER': 'postgres',
-    #     'PASSWORD': '217217',
-    #     'HOST': 'localhost',
-    #     'PORT': '5432'
-
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'TODO_Proj',
+        'USER': 'postgres',
+        'PASSWORD': '217217',
+        'HOST': 'localhost',
+        'PORT': '5432'
+
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
